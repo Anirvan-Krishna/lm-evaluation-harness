a44fe64f48930ca5e798b890aa840838ffd7cee9
Anirvan-Krishna <anirvankrishna@gmail.com>
2024-06-15 22:07:34 +0530

    Update task.py

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -268,7 +268,7 @@ class Task(abc.ABC):
         self.dataset = datasets.load_dataset(
             path=self.DATASET_PATH,
             name=self.DATASET_NAME,
-            data_files= self.DATA_FILES
+            data_files= self.DATA_FILES,
             data_dir=data_dir,
             cache_dir=cache_dir,
             download_mode=download_mode,
